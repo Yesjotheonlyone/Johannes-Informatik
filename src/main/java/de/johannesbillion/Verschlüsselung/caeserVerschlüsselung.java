f317553790efc1f303c12c46f33bf776296141ee
Johannes-Laptop <johannes@laptop>
2023-02-06 15:03:31 +0100

    "first caeser verschlüsselung"

--- src/main/java/de/johannesbillion/Verschlüsselung/caeserVerschlüsselung.java
+++ src/main/java/de/johannesbillion/Verschlüsselung/caeserVerschlüsselung.java
@@ -1,66 +1,39 @@
 package de.johannesbillion.Verschlüsselung;
 
-import de.johannesbillion.arrayExercises.Array;
 import basis.*;
-
-public class caeserVerschlüsselung {
-    public static void main(String[] args) {
-        new Array();
-    }
-
-
-    public class caeser_einfach implements KnopfLauscher
-    {
-        private Fenster fenster;
-        private BeschriftungsFeld ueberSchriftEing, ueberSchriftAusg;
-        private TextFeld eingabeFeld, ausgabeFeld;
-        private Knopf code, decode;
-        private String eingabe, ausgabe,abc,codierterText,decodierterText;
-        private char aktuellerBuchstabe,neuerBuchstabe;
-        private int key,positionImAlphabet,neuePositionImAlphabet;
-        public caeser_einfach()
-        {
-            fenster = new Fenster(500,150);
-
-            ueberSchriftEing = new BeschriftungsFeld("Hier den Text eingeben",10,10,200,10);
-            eingabeFeld = new TextFeld(10,30,200,20);
-
-            ueberSchriftAusg = new BeschriftungsFeld("Hier steht der verschlüsselte Text",220,10,300,10);
-            ausgabeFeld = new TextFeld(220,30,200,20);
-
-            code= new Knopf("Verschlüsseln",10,60,150,20);
-            code.setzeKnopfLauscher(this);
-
-            decode = new Knopf("Entschlüsseln",220,60,150,20);
-            decode.setzeKnopfLauscher(this);
-            this.main();
-
-            abc="abcdefghijklmnopqrstuvwxyz";
-            key=3;
-            codierterText="";
-            decodierterText="";
-
-        }
-
-        public void main()
-        {
-
-        }
-
-        public void verschluesseln()
-        {
-
-        }
-        public void entschluesseln()
-        {
-
-        }
-
-        @Override
-        public void bearbeiteKnopfDruck(Knopf k){
-
-            }
-        }
-
+class verschluessung
+{
+    //private Fenster f;
+    //private TextFeld t;
+    private int aktuellePosition,neuePosition,verschiebung;
+    //private char[]alphabet1;
+    //private char[]text;
+    private char aktuelleBuchstabe,neueBuchstabe;
+    private String texteingabe;
+    private String alphabet;
+    private String neu1;
+    public verschluessung(){
+    //f = new Fenster(200,200);
+    //t = new TextFeld(50,50,100,100);
+        texteingabe = "Pflanze";
+        verschiebung = 3;
+        alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ÄÖÜäöü.,-;abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ÄÖÜäöü.,-;";
+        aktuellePosition = 0;
+        neu1="";
+        neuePosition = 0;
+    }public void verschluesseln(){
+    for(int i=0; i<texteingabe.length(); i++){
+        aktuelleBuchstabe = texteingabe.charAt(i);
+        System.out.println("Aktueller Buchstabe: "+aktuelleBuchstabe);
+        aktuellePosition = alphabet.indexOf(aktuelleBuchstabe);
+        System.out.println("Aktuelle Position: "+aktuellePosition);neuePosition = aktuellePosition + verschiebung;
+        System.out.println("Verschiebung: "+verschiebung);
+        System.out.println("Neue Position: "+neuePosition);
+        neueBuchstabe = alphabet.charAt(neuePosition);
+        System.out.println("Neuer Buchstabe: "+neueBuchstabe);
+        neu1= neu1+neueBuchstabe;
+        System.out.println("Verschlüsselt: "+neu1);
     }
 }
+}
+
---
+++ src/main/java/de/johannesbillion/Verschlüsselung/secondTryEncription.java
@@ -0,0 +1,9 @@
+package de.johannesbillion.Verschlüsselung;
+
+import de.johannesbillion.GUI;
+
+public class secondTryEncription {
+    public static void main(String[] args) {
+}
+
+}
